77e946e6c67d2d0b32c205b286a30103bba9da80
Maksim Lanies <91416159+mlanies@users.noreply.github.com>
2023-04-12 11:56:11 +0300

    Update cloudflare_run_python_mac.py

--- mac/cloudflare_run_python_mac.py
+++ mac/cloudflare_run_python_mac.py
@@ -1,66 +1,66 @@
-import subprocess
-import sys
-import datetime
-import os
 import pystray
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction
+from tkinter import *
+import subprocess
+import random
 
-# Параметры запуска службы Cloudflare
-cloudflare_params = ["service", "cloudflared", "start"]
+# Класс для иконки в трее
+class App:
+    def __init__(self):
+        self.icon = pystray.Icon("rdp")
+        self.icon.menu = pystray.Menu(pystray.MenuItem(
+            'Exit', lambda icon, item: icon.stop()
+        ))
+        self.icon.run()
+        self.address_history = []  # список сохраненных адресов
+        self.port_history = []  # список сохраненных портов
 
-# Параметры запуска RDP клиента
-rdp_params = ["xfreerdp", "/u:<username>", "/p:<password>", "/v:<server>"]
+    # Функция запуска программы и получения случайного порта
+    def start_program(self):
+        port = str(random.randint(1100, 1200))
+        hostname = self.get_hostname()
+        cmd = f'cloudflared access rdp --hostname {hostname} --url rdp://localhost:{port}'
+        subprocess.Popen(cmd.split())
+        self.address_history.append(hostname)
+        self.port_history.append(port)
 
-app = QApplication(sys.argv)
+    # Функция отображения окна для ввода hostname
+    def get_hostname(self):
+        self.hostname = ""
+        def submit():
+            self.hostname = entry.get()
+            root.destroy()
 
-# Создаем иконку в трее
-tray_icon = QSystemTrayIcon(QIcon("icon.png"), app)
+        root = Tk()
+        root.title("Enter hostname")
+        entry = Entry(root, width=30)
+        entry.pack(side=LEFT, padx=5)
+        button = Button(root, text="OK", command=submit)
+        button.pack(side=LEFT, padx=5)
+        root.mainloop()
 
-# Создаем меню для иконки в трее
-menu = QMenu()
+        return self.hostname
 
-# Функция для запуска службы Cloudflare
-def start_cloudflare():
-    subprocess.run(cloudflare_params)
+    # Функция запуска RDP
+    def start_rdp(self, hostname, port):
+        cmd = f'xfreerdp /v:{hostname}:{port}'
+        subprocess.Popen(cmd.split())
 
-# Функция для запуска RDP клиента
-def start_rdp():
-    subprocess.run(rdp_params)
+app = App()
 
-# Функция для отображения текущего времени на экране
-def show_time():
-    now = datetime.datetime.now().strftime("%H:%M:%S")
-    tray_icon.setToolTip(now)
+# Добавление пунктов меню в иконку в трее
+app.icon.menu.append(pystray.MenuItem(
+    'Start program', lambda icon, item: app.start_program()
+))
+app.icon.menu.append(pystray.MenuItem(
+    'History', pystray.Menu(
+        *[
+            pystray.MenuItem(f'{hostname}:{port}', lambda icon, item: app.start_rdp(hostname, port))
+            for hostname, port in zip(app.address_history, app.port_history)
+        ]
+    )
+))
 
-# Добавляем действия в меню
-start_cloudflare_action = QAction("Start Cloudflare", menu)
-start_cloudflare_action.triggered.connect(start_cloudflare)
-
-start_rdp_action = QAction("Start RDP", menu)
-start_rdp_action.triggered.connect(start_rdp)
-
-show_time_action = QAction("Show Time", menu)
-show_time_action.triggered.connect(show_time)
-
-exit_action = QAction("Exit", menu)
-exit_action.triggered.connect(app.quit)
-
-menu.addAction(start_cloudflare_action)
-menu.addAction(start_rdp_action)
-menu.addAction(show_time_action)
-menu.addSeparator()
-menu.addAction(exit_action)
-
-tray_icon.setContextMenu(menu)
-
-# Функция для выхода из программы
-def on_exit(icon, item):
-    tray_icon.setVisible(False)
-    app.quit()
-
-# Создаем иконку в трее с обработчиками событий
-tray_icon = pystray.create_systray(icon="icon.png", menu=menu)
-tray_icon.run(on_exit)
-
-sys.exit(app.exec_())
+# Запуск GUI
+app.icon.visible = True
+app.icon.systray = True
+app.icon.run()
