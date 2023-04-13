d922806e12bccddc5b7af5edc73a4f7761abd2af
Maksim Lanies <91416159+mlanies@users.noreply.github.com>
2023-04-13 12:37:12 +0300

    Update cloudflare_run_python_win.py

--- win/cloudflare_run_python_win.py
+++ win/cloudflare_run_python_win.py
@@ -1,29 +1,110 @@
-from tkinter import *
-import os
-import time
-import random
-from pystray import Menu, MenuItem
-import pystray
+import sys
+from PyQt5.QtWidgets import QApplication, QMenu, QSystemTrayIcon, QAction, QDialog, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QListWidget, QMessageBox, QCheckBox
+from PyQt5.QtGui import QIcon
 
-def cloudflare1():
-url = input("Введите URL: ")
-port = random.randint(1100, 1200)
-os.system(
-'start cmd /c cloudflared access rdp --hostname "{}" --url rdp://localhost:{}'
-.format(url, port))
-time.sleep(4)
-os.system('start cmd /c mstsc /v:localhost:{}'.format(port))
+class App(QDialog):
+    def __init__(self):
+        super().__init__()
 
-def on_quit():
-tray.icon = None
-root.quit()
+        # Create tray icon
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(QIcon('icon.png'))
+        self.tray_icon.setVisible(True)
+        self.tray_icon.setToolTip('Server Manager')
 
-menu = Menu(MenuItem('Cloudflare 1', cloudflare1),
-MenuItem('Quit', on_quit))
+        # Create menu
+        menu = QMenu(self)
+        enter_url = QAction("Ввести URL:", self)
+        enter_url.triggered.connect(self.show_url_dialog)
+        menu.addAction(enter_url)
 
-image = Image.open("icon.png")
-tray = pystray.Icon("name", image, "title", menu)
-tray.run()
+        server_list = QAction("Список серверов", self)
+        server_list.triggered.connect(self.show_server_list)
+        menu.addAction(server_list)
 
-root = Tk()
-root.mainloop()
+        settings = QAction("Настройки", self)
+        settings.triggered.connect(self.show_settings)
+        menu.addAction(settings)
+
+        exit_action = QAction("Выход", self)
+        exit_action.triggered.connect(self.exit)
+        menu.addAction(exit_action)
+
+        self.tray_icon.setContextMenu(menu)
+
+        # Create other widgets       
+        self.url_list = QListWidget()
+        
+        self.autostart_checkbox = QCheckBox('Автозапуск программы', self)
+
+        self.setWindowTitle('Server Manager')
+
+        # Add widgets to layout
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.url_list)
+        
+        settings_layout = QHBoxLayout()
+        settings_layout.addWidget(self.autostart_checkbox)
+        main_layout.addLayout(settings_layout)
+
+        self.setLayout(main_layout)
+
+    def show_url_dialog(self):
+        # Create URL dialog
+        url_dialog = QDialog(self)
+        url_dialog.setWindowTitle('Введите URL')
+        url_dialog.setGeometry(100, 100, 50, 100)
+
+        url_label = QLabel('URL:', url_dialog)
+        url_label.move(5, 5)
+
+        url_edit = QLineEdit(url_dialog)
+        url_edit.move(5, 25)
+
+        ok_button = QPushButton('OK', url_dialog)
+        ok_button.move(5, 45)
+        ok_button.clicked.connect(lambda: self.add_url(url_edit.text(), url_dialog))
+
+        url_dialog.exec_()
+
+    def add_url(self, url, url_dialog):
+        # Add URL to list
+        self.url_list.addItem(url)
+        url_dialog.hide()
+
+    def show_server_list(self):
+        # Get selected URL
+        selected_url = self.url_list.currentItem().text()
+        
+        # Generate random port number between 1100 and 1200
+        port = random.randint(1100, 1200)
+
+        # Run cmd command
+        cmd_command = f'cloudflared access rdp --hostname "{selected_url}-host" --url rdp://localhost:{port}'
+        os.system(cmd_command)
+
+        # Run mstsc command
+        mstsc_command = f'start cmd /c mstsc /v:localhost:{port}'
+        os.system(mstsc_command)
+
+    def show_settings(self):
+        # Create settings dialog
+        settings_dialog = QDialog(self)
+        settings_dialog.setWindowTitle('Настройки')
+        settings_dialog.setGeometry(100, 100, 300, 100)
+
+        autostart_layout = QHBoxLayout()
+        autostart_layout.addWidget(self.autostart_checkbox)
+        settings_dialog.setLayout(autostart_layout)
+
+        settings_dialog.exec_()
+
+    def exit(self):
+        # Close application
+        self.tray_icon.setVisible(False)
+        QApplication.quit()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
